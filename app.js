29a8b5858c01806f43efb54818e6cf598752fda0
bharatsdrv <bharatsdruv@gmail.com>
2021-09-01 00:07:40 +0530

    added routes and MongoDB Database connection

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express=require('express')
 const app=express()
 require('dotenv').config()
+//import Routes
+const userRoutes=require('./routes/user')
+// import mongoose
+const mongoose = require('mongoose');
+// load env variables
+const dotenv = require('dotenv');
+dotenv.config()
 
-app.get('/',(req,res)=>{
-    res.send('hello from node');
+//db connection
+mongoose.connect(
+  process.env.MONGO_URI,
+  {useNewUrlParser: true}
+)
+.then(() => console.log('DB Connected'))
+
+mongoose.connection.on('error', err => {
+  console.log(`DB connection error: ${err.message}`)
 });
+
+//routes middleware
+app.use(userRoutes)
 
 const port=process.env.PORT || 8000
 
