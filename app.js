0353a175e64e8e471de726bc424a50cce14d97e6
bharatsdrv <bharatsdruv@gmail.com>
2021-09-06 22:40:40 +0530

    added signup functionality with MongoDB and error handling

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express=require('express')
 const app=express()
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+
+
 require('dotenv').config()
 //import Routes
 const userRoutes=require('./routes/user')
@@ -15,6 +20,11 @@ mongoose.connect(
   {useNewUrlParser: true}
 )
 .then(() => console.log('DB Connected'))
+
+//middleware
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cookieParser())
 
 mongoose.connection.on('error', err => {
   console.log(`DB connection error: ${err.message}`)
